c47f874470eb7475bffc84d4a1f610cfffa59c3b
hamidusman <abdulhamidusman218@gmail.com>
2024-10-16 21:32:30 +0100

    booking and rating serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import Service, Booking, Rating
+
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = '__all__'
 
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+    
 class StatSerializer(serializers.Serializers):
     total_bookings = serializers.IntegerField()
     jobs_completed = serializers.IntegerField()
