670e678ac578b1494656f263b390e78353820f65
hamidusman <abdulhamidusman218@gmail.com>
2024-11-24 22:20:35 +0100

    removed debug toolbar

--- Fixit_API/settings.py
+++ Fixit_API/settings.py
@@ -31,14 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-INTERNAL_IPS = [
-    '127.0.0.1',  # Add your local IP address here
-]
-
-# Optional: Debug Toolbar configuration
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,  # Show toolbar only in DEBUG mode
-}
 # Application definition
 
 INSTALLED_APPS = [
@@ -65,8 +57,7 @@ MIDDLEWARE = [
     
     'django.middleware.cache.UpdateCacheMiddleware',  # Adds caching at the start of request processing
     'django.middleware.cache.FetchFromCacheMiddleware',
-    
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -76,23 +67,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-]
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.alerts.AlertsPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 
 USE_SWAGGER=True
